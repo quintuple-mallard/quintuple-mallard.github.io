fc94bf2a233f7bfa0321f9ffb434a53bae8e30ef
J R M <jwrmmine@protonmail.com>
2025-08-10 10:40:50 +0200

    remove unused hljs

--- posts/script.js
+++
@@ -1,5 +0,0 @@
-import hljs from './assets/js/@highlightjs/cdn-assets/es/core.js';
-import javascript from './assets/js/@highlightjs/cdn-assets/es/languages/javascript.min.js';
-
-hljs.registerLanguage('javascript', javascript);
-document.querySelectorAll('pre code').forEach(el => hljs.highlightElement(el));
